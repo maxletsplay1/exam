f1475104b311fa43eb6cef5619aa50a233cdc806
Maxim Olennikov <105488018+maxletsplay1@users.noreply.github.com>
2024-04-27 09:05:11 +0700

    require поля для формы, валидация файла на фронте

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -21,15 +21,15 @@
                         <div class="modal-body">
                             <div class="mb-3">
                                 <label for="exampleFormControlInput1" class="form-label">Номер машины</label>
-                                <input type="text" name="carnum" class="form-control" id="exampleFormControlInput1" placeholder="X 000 XX 00">
+                                <input type="text" name="carnum" required class="form-control" id="exampleFormControlInput1" placeholder="X 000 XX 00">
                             </div>
                             <div class="mb-3">
                                 <label for="exampleFormControlInput2" class="form-label">Описание нарушения</label>
-                                <input type="text" name="problem" class="form-control" id="exampleFormControlInput2">
+                                <input type="text" name="problem" required class="form-control" id="exampleFormControlInput2">
                             </div>
                             <div class="mb-3">
                                 <label for="exampleFormControlInput3" class="form-label">Фото нарушения</label>
-                                <input type="file" name="file" class="form-control" id="exampleFormControlInput3">
+                                <input type="file" name="file" accept="image/jpeg" required class="form-control" id="exampleFormControlInput3">
                             </div>
                         </div>
                         <input type="hidden" name="user_id" value="{{Auth::user()->id}}">
